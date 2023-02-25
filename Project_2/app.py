f7e88947fac05e631ce97cca5bf4517aba2df4a9
S Dheeraj Reddy <99827504+Dheeraj-2022@users.noreply.github.com>
2023-02-26 02:52:42 +0530

    Add files via upload

--- Project_2/app.py
+++ Project_2/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("S Dheeraj Reddy")
 st.header("Data Science Internship ")
-
+st.sidebar('Choose a demo')
 st.snow()
 
 b_click=st.button("Click Me!!")
@@ -10,3 +10,5 @@ b_click=st.button("Click Me!!")
 if b_click == True:
     st.subheader("It is Intresting to work in DS_Intership")
     st.balloons()
+
+
